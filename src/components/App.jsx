73b7177f2983dd0ece0e084057819453c2769a44
BondAndrii <104005997+BondAndrii@users.noreply.github.com>
2022-11-02 21:34:54 +0200

    ColorPicker 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,23 @@
 import React from "react";
-// import Counter from "./Counter";
-// import Dropdown from "./Dropdown";
-// import ColorPicker from "./ColorPicker";
+import Counter from "./Counter";
+import Dropdown from "./Dropdown";
+import ColorPicker from "./ColorPicker";
 import TodoList from "./TodoList";
 
-// const colorPickerOptions = [
-//   { label: 'red', color:'#F44336'},
-//   { label: 'green', color:'#4CAF50'},
-//   { label: 'blue', color:'#2196F3'},
-//   { label: 'grey', color:'#607D8b'},
-//   { label: 'pink', color:'#E91E63'},
-//   { label: 'indigo', color:'#3F51B5'},
-// ];
+const colorPickerOptions = [
+  { label: 'red', color:'#F44336'},
+  { label: 'green', color:'#4CAF50'},
+  { label: 'blue', color:'#2196F3'},
+  { label: 'grey', color:'#607D8b'},
+  { label: 'pink', color:'#E91E63'},
+  { label: 'indigo', color:'#3F51B5'},
+];
 export const App = () => (
   <>
     <h1>Стан компонента</h1>
-    {/* <Counter initialValue={10} />
+    <Counter initialValue={10} />
     <Dropdown />
-    <ColorPicker options={colorPickerOptions} /> */}
+    <ColorPicker options={colorPickerOptions} />
     <TodoList/>
   </>
 
